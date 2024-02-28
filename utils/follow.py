dd8ff8d5ec69e63ac8ca736c126a200d1e2564cc
Tomás Manzur <tmanzur@minimalistech.com>
2024-02-28 14:07:26 -0300

    updates follow module

--- utils/follow.py
+++ utils/follow.py
@@ -1,20 +1,12 @@
 from login import bot
 
 user_id = bot.get_user_id_from_username("cyberpunk_art")
+my_user_id = bot.get_user_id_from_username("cyberpunk.art.posts")
+my_follows = set(bot.get_user_following(my_user_id))  # Convert list to set for efficient lookup
+followers_list = bot.get_user_followers(user_id=user_id, nfollows=100)
+filtered_followers_list = [user for user in followers_list if user not in my_follows]
 
 try:
-    followers_list = bot.get_user_followers(user_id=user_id, nfollows=100)
-except Exception:
-    print(Exception)
-
-try:
-    bot.follow_users(followers_list)
-except Exception:
-    print(Exception)
-
-#unfollow non followers
-#bot.unfollow_non_followers(n_to_unfollows=50)
-
-#get list of followings of an user
-#following_list = bot.get_user_following(user_id="", nfollows=100)
-
+    bot.follow_users(filtered_followers_list)
+except Exception as e:
+    print(e)
